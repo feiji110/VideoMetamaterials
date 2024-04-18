f3bc11500adf38c2f790cf7183d00a943a40ec23
hengda <ghd@nudt.edu.cn>
2024-04-18 19:52:06 +0800

    添加循环脚本

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@ export RANK=0
 export WORLD_SIZE=1
 export MASTER_ADDR=localhost
 export MASTER_PORT=12345
-accelerate launch main.py
 
-
+for ((i=1; i<=100; i ++))
+do
+	echo $i
+    accelerate launch main.py
+done
